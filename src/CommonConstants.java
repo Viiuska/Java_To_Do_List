c8fffc5af9e2c66318ebc75f85f802c9107b0198
Viiuska <87257685+Viiuska@users.noreply.github.com>
2026-01-12 08:52:59 +0200

    Init

---
+++ src/CommonConstants.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+public class CommonConstants {
+
+    public static final Dimension GUI_SIZE = new Dimension(540,760);
+
+
+}
---
+++ src/ToDoListGui.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class ToDoListGui extends JFrame {
+    public ToDoListGui(){
+        super("To Do List app");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setPreferredSize(CommonConstants.GUI_SIZE);
+        pack();
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setLayout(null);
+    }
+
+}
