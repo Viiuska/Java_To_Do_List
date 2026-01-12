051c3e435a9a59430e9d46eabe6d28e13d1e4262
Viiuska <87257685+Viiuska@users.noreply.github.com>
2026-01-12 09:23:28 +0200

    Task component panel, scroll added to Frame

--- src/CommonConstants.java
+++ src/CommonConstants.java
@@ -6,5 +6,7 @@ public class CommonConstants {
 
     public static final Dimension BANNER_SIZE = new Dimension(GUI_SIZE.width, 50);
 
+    public static final Dimension TASKPANEL_SIZE = new Dimension(GUI_SIZE.width - 30, GUI_SIZE.height - 175);
+
 
 }
--- src/ToDoListGui.java
+++ src/ToDoListGui.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 
 public class ToDoListGui extends JFrame {
+
+    private JPanel taskPanel, taskComponentPanel;
+
     public ToDoListGui(){
         super("To Do List app");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -19,6 +22,21 @@ public class ToDoListGui extends JFrame {
                 (CommonConstants.GUI_SIZE.width - bannerLabel.getPreferredSize().width)/2,
                 15, CommonConstants.BANNER_SIZE.width, CommonConstants.BANNER_SIZE.height
         );
+
+        taskPanel = new JPanel();
+
+        taskComponentPanel = new JPanel();
+        taskComponentPanel.setLayout(new BoxLayout(taskComponentPanel, BoxLayout.Y_AXIS));
+        taskPanel.add(taskComponentPanel);
+
+        JScrollPane scrollPane = new JScrollPane(taskPanel);
+        scrollPane.setBounds(8, 70, CommonConstants.TASKPANEL_SIZE.width, CommonConstants.TASKPANEL_SIZE.height);
+        scrollPane.setMaximumSize(CommonConstants.TASKPANEL_SIZE);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+
         this.getContentPane().add(bannerLabel);
+        this.getContentPane().add(scrollPane);
     }
 }
