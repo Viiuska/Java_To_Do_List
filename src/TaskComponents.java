e43a735e6370a08deb959b970a37753dc7e8e2dc
Viiuska <87257685+Viiuska@users.noreply.github.com>
2026-01-12 10:45:58 +0200

    Finalizing GUI

--- src/TaskComponents.java
+++ src/TaskComponents.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class TaskComponents extends JPanel implements ActionListener {
     private JCheckBox checkBox;
@@ -17,15 +20,30 @@ public class TaskComponents extends JPanel implements ActionListener {
         this.parentPanel = parentPanel;
 
         taskField = new JTextPane();
+        taskField.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
         taskField.setPreferredSize(CommonConstants.TASKFIELD_SIZE);
         taskField.setContentType("text/html");
+        taskField.addFocusListener(new FocusListener() {
+            //Show which task field is currently edited
+            @Override
+            public void focusGained(FocusEvent e) {
+                taskField.setBackground(Color.WHITE);
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                taskField.setBackground(null);
+            }
+        });
 
         checkBox = new JCheckBox();
         checkBox.setPreferredSize(CommonConstants.CHECKBOX_SIZE);
+        checkBox.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         checkBox.addActionListener(this);
 
         deleteButton = new JButton("X");
         deleteButton.setPreferredSize(CommonConstants.DELETEBUTTON_SIZE);
+        deleteButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         deleteButton.addActionListener(this);
 
         add(checkBox);
@@ -39,12 +57,10 @@ public class TaskComponents extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if(checkBox.isSelected()){
             String taskText = taskField.getText().replaceAll("<[^>]*>", "");
-
             taskField.setText("<html><s>"+ taskText+ "</s></html>");
 
         } else if (!checkBox.isSelected()){
             String taskText = taskField.getText().replaceAll("<[^>]*>", "");
-
             taskField.setText(taskText);
         }
 
--- src/ToDoListGui.java
+++ src/ToDoListGui.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -20,6 +21,7 @@ public class ToDoListGui extends JFrame implements ActionListener {
 
     private void addGuiComponents(){
         JLabel bannerLabel = new JLabel("To Do List");
+        bannerLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 26));
         bannerLabel.setBounds(
                 (CommonConstants.GUI_SIZE.width - bannerLabel.getPreferredSize().width)/2,
                 15, CommonConstants.BANNER_SIZE.width, CommonConstants.BANNER_SIZE.height
@@ -33,11 +35,18 @@ public class ToDoListGui extends JFrame implements ActionListener {
 
         JScrollPane scrollPane = new JScrollPane(taskPanel);
         scrollPane.setBounds(8, 70, CommonConstants.TASKPANEL_SIZE.width, CommonConstants.TASKPANEL_SIZE.height);
+        scrollPane.setBorder(BorderFactory.createLoweredBevelBorder());
         scrollPane.setMaximumSize(CommonConstants.TASKPANEL_SIZE);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
+        //Scroll bar speed
+        JScrollBar verticalScrollBar = scrollPane.getVerticalScrollBar();
+        verticalScrollBar.setUnitIncrement(20);
+
         JButton addTaskButton = new JButton("Add Task");
+        addTaskButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 18));
+        addTaskButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         addTaskButton.setBounds(-5, CommonConstants.GUI_SIZE.height -88,
                 CommonConstants.ADDTASKBUTTON_SIZE.width, CommonConstants.ADDTASKBUTTON_SIZE.height);
         addTaskButton.addActionListener(this);
@@ -54,6 +63,11 @@ public class ToDoListGui extends JFrame implements ActionListener {
         if(command.equalsIgnoreCase("Add Task")) {
            TaskComponents taskComponents = new TaskComponents(taskComponentPanel);
            taskComponentPanel.add(taskComponents);
+
+           if(taskComponentPanel.getComponentCount() > 1){
+               TaskComponents previousTask = (TaskComponents) taskComponentPanel.getComponent(taskComponentPanel.getComponentCount() - 2);
+               previousTask.getTaskField().setBackground(null);
+           }
 
            taskComponents.getTaskField().requestFocus();
            repaint();
